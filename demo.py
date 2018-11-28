700613f8a4f5bb4c8c5e35147dfc08ab9a9c425c
lisgok <panpengok@126.com>
2018-11-28 10:22:26 +0800

    updata

--- demo.py
+++ demo.py
@@ -10,10 +10,10 @@ import pandas as pd
 import math
 import os
 from pyspark.sql import SQLContext
-from pyspark.sql import SparkSession 
+from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from time import time
-
+from pyspark.ml.linalg import Vectors
 
 if __name__ == "__main__":
     print("======")
@@ -41,8 +41,16 @@ if __name__ == "__main__":
     two_test = spark.sql("select * from global_temp.people")
     print("took %.2f seconds for" % ((time()-start )))
 
-
+    # 将数据转换成标签数据
+    def parsePoint(line):
+        return (line[-1], Vectors.dense(line[:-1]))
+    # # parsedData = my_test.map(parsePoint)
+    parsedData = my_test.rdd.map(parsePoint)
+    df = spark.createDataFrame(parsedData, ["label", "features"])
+    hasattr(parsedData, "toDF")
+    # df = spark.createDataFrame(parsedData, ["label", "features"])
     # 读取spark的训练格式
+
 
     # $example on$
     # Load the data stored in LIBSVM format as a DataFrame.
